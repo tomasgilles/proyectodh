174b34b3a891dbd239fb5a782eccc8a0456f5146
Marcos <marcos.letemendia@gmail.com>
2019-08-30 19:56:09 -0300

    primeros pasos

--- README.md
+++
@@ -1,2 +0,0 @@
-# proyectodh
-proyecto DH
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
+  </head>
+  <body>
+    <div class="main">
+      <div class="foto-estadio col-md-10 text-center">
+        <img src="fotos/estadio.jpg" class="img-fluid" alt="Stadium">
+      </div>
+    </div>
+  </body>
+</html>
