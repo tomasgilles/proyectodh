9a2d1c913ca88688b08eb8bb9c2b73d67d20f43e
tomasgilles <tomas.gilles@gmail.com>
2019-09-02 18:27:44 -0300

    gilles

---
+++ register.php
@@ -0,0 +1,52 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta name="viewport" content="width=320px, initial-rule=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="techo">
+      <h1>Mi Cuenta</h1>
+    </div>
+    <div class="cuerpo">
+      <div class="cuerpo2">
+        <div class="navbar">
+          <nav class="nav nav-pills nav-fill">
+            <a class="nav-item nav-link active show" href="#login">Login</a>
+            <a class="nav-item nav-link active show" href="#registro">Registro</a>
+        </div>
+        <div class="registro" id="registro">
+          <h2>Registro</h2>
+          <form>
+            <div class="form-group">
+              <label for="formGroupExampleInput"></label>
+              <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Nombre*" required>
+            </div>
+            <div class="form-group">
+              <label for="formGroupExampleInput2"></label>
+              <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Apellido*" required>
+            </div>
+            <div class="form-group">
+              <label for="formGroupExampleInput2"></label>
+              <input type="email" class="form-control" id="formGroupExampleInput3" placeholder="Email*" required>
+            </div>
+            <div class="form-group">
+              <label for="formGroupExampleInput2"></label>
+              <input type="password" class="form-control" id="formGroupExampleInput4" placeholder= "Password*" required>
+            </div>
+            <div class="form-group">
+              <label for="formGroupExampleInput2"></label>
+              <input type="password" class="form-control" id="formGroupExampleInput5" placeholder="Confirm password*" required>
+            </div>
+            <button type="button" class="btn btn-outline-primary btn-lg btn-block">Crear Cuenta</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
