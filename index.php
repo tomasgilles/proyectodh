57a89f7507c7b2ed3bd522e093079852784d0169
Marcos <marcos.letemendia@gmail.com>
2019-09-03 12:57:45 -0300

    updates

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
@@ -13,7 +15,7 @@
 
     <div class="container-fluid main"> <!-- GENERAL -->
 
-       <div class="first container-fluid">  <!-- FOTO ESTADIO + GIF ANIMADO-->
+       <div class="first container-fluid px-0">  <!-- FOTO ESTADIO + GIF ANIMADO-->
 
         <div class="foto-estadio">
           <h1>foto estadio</h1>
@@ -21,65 +23,46 @@
         </div>
 
         <div class="gif-animado col-md-8 h-50"> <!-- gif animado -->
-          <h1 class="text-center text-white">FUTBOL Y NADA +</h1>
-          <div id="MagicCarousel1" class="carousel slide w-50 mx-auto p-0" data-ride="carousel">
-            <div class="carousel-inner" role="listbox">
-              <div class="carousel-item active">
-                <img class="d-block w-100" src="fotos/botines/botines-nike.jpg" alt="Primer slide">
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="fotos/botines/botines-adidas.jpg" alt="Segundo slide">
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="fotos/botines/botines-puma.jpg" alt="Tercero slide">
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="fotos/botines/botines-under-armour.jpg" alt="Cuarto slide">
-              </div>
-              <a class="carousel-control-prev" href="#MagicCarousel1" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon bg-dark"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="carousel-control-next" href="#MagicCarousel1" role="button" data-slide="next">
-                <span class="carousel-control-next-icon bg-dark"></span>
-                <span class="sr-only">Next</span>
-              </a>
-            </div>
-          </div>
+          <div class="">
+            <iframe src="https://giphy.com/embed/jPM6J4te3D3WJuGmvq" width="100%" height="100%"  frameBorder="0" class="giphy-embed" allowFullScreen>
+
+            </iframe>
+          </div><p>
+            <a href="register.php"></a>
+          </p>
         </div>
 
       </div>
 
-      <div class="destacados text-center container-fluid mt-5 mb-5"> <!-- SLIDER con Camisetas destacados -->
-        <br><br> <h1>NEW ARRIVALS</h1> <br>
-        <div class="fotos-camisetas col-sm-12 col-md-6 col-lg-3 mb-md-3 float-left border-right border-dark">
+      <div class="destacados text-center container-fluid mt-3 p-4"> <!-- SLIDER con Camisetas destacados -->
+        <h1>NEW ARRIVALS</h1>
+        <div class="mt-1 fotos-camisetas col-xs-12 col-sm-6 col-md-6 col-lg-3 mb-md-3 float-left border-right border-dark">
           <img class="w-100" src="fotos/camisetas/camiseta-river.jpg" alt="">
         </div>
-        <div class="fotos-camisetas col-sm-12 col-md-6 col-lg-3 mb-md-3 float-left border-right border-dark">
+        <div class="mt-1 fotos-camisetas col-xs-12 col-sm-6 col-md-6 col-lg-3 mb-md-3 float-left border-right border-dark">
           <img class="w-100" src="fotos/camisetas/camiseta-boca.jpg" alt="">
         </div>
-        <div class="fotos-camisetas col-sm-12 col-md-6 col-lg-3 mb-md-3 float-left border-right border-dark">
+        <div class="mt-1 fotos-camisetas col-xs-12 col-sm-6 col-md-6 col-lg-3 mb-md-3 float-left border-right border-dark">
           <img class="w-100" src="fotos/camisetas/camiseta-mancity.jpg" alt="">
         </div>
-        <div class="fotos-camisetas col-sm-12 col-md-6 col-lg-3 mb-md-3 float-left">
+        <div class="mt-1 fotos-camisetas col-xs-12 col-sm-6 col-md-6 col-lg-3 mb-md-3 float-left">
           <img class="w-100" src="fotos/camisetas/camiseta-atleticom.jpg" alt="">
         </div>
       </div>
 
-      <div class="promo container-fluid mt-5"> <!-- PROMO KIT BARCELONA -->
+      <div class="promo container-fluid mt-3"> <!-- PROMO KIT BARCELONA -->
         <h1 class="text-center">PROMO KIT BARCELONA</h1>
         <section class="parrafo float-left col-6">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </section>
 
         <section class="imagen-parrafo float-left col-6">
-          <br>
           <img src="fotos/promo-barcelona.jpg" alt="" class="img-fluid">
         </section>
       </div>
 
-      <div class="ofertas text-center container-fluid"> <!-- SLIDER con ofertas -->
-        <br><br> <h1>SLIDER con ofertas</h1> <br><br>
+      <div class="ofertas text-center container-fluid mt-0"> <!-- SLIDER con ofertas -->
+        <h1>SLIDER con ofertas</h1>
       </div>
 
     </div>
@@ -92,5 +75,8 @@
 
     </div>
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
