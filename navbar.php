d960402fdf6531420217e9ee89d8c5a0bca0c141
tomasgilles <tomas.gilles@gmail.com>
2019-09-05 11:03:48 -0300

    l

--- navbar.php
+++ navbar.php
@@ -28,10 +28,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"> Botines</a>
+          <a class="nav-link" href="#">Botines</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -45,7 +45,7 @@
         </ul>
       <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Login <i class="far fa-user"></i></button> 
+        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Login <i class="far fa-user"></i></button>
         <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Registro <i class="fa fa-shopping-cart"></i></button>
       </form>
     </div>
