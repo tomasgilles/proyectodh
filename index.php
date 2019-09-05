0478a80226b51b3fbafc927fc3b90ba04661a2d9
Marcos <marcos.letemendia@gmail.com>
2019-09-05 10:49:59 -0300

    incluir footer y navbar

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
     <title></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css" integrity="sha256-piqEf7Ap7CMps8krDQsSOTZgF+MU/0MPyPW2enj5I40=" crossorigin="anonymous" />
+
+
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
 
     <!-- NAVBAR -->
-    <h1>NAVBAR</h1>
+    <?php include("navbar.php"); ?>
 
     <div class="container-fluid main"> <!-- GENERAL -->
 
@@ -72,7 +75,7 @@
     </div>
 
     <!-- FOOTER -->
-    <h1>FOOTER</h1>
+    <?php include("footer.php"); ?>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
