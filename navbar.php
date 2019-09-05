7c63648d511d76eb2847572b39d6208243f35302
tomasgilles <tomas.gilles@gmail.com>
2019-09-05 12:32:22 -0300

    Update

--- navbar.php
+++ navbar.php
@@ -22,14 +22,39 @@
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="#">Remeras</a>
             <a class="dropdown-item" href="#">Camperas</a>
-
-        </li>
+          </div>
+          </li>
         </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Login <i class="far fa-user"></i></button>
-        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Registro <i class="fa fa-shopping-cart"></i></button>
-      </form>
+        <form class="form-inline my-2 my-lg-0" id="formnavbar">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+
+        <div class="dropdown">
+          <button class="btn btn-primary dropdown-toggle" type="button" id="loginFromButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Login
+          </button>
+          <form class="dropdown-menu dropdown-menu p-4" aria-labelledby="loginFromButton">
+            <div class="form-group">
+              <label for="exampleDropdownFormEmail2">Email address</label>
+              <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
+            </div>
+            <div class="form-group">
+              <label for="exampleDropdownFormPassword2">Password</label>
+              <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
+            </div>
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" id="dropdownCheck2">
+              <label class="form-check-label" for="dropdownCheck2">
+                Remember me
+              </label>
+            </div>
+            <button type="submit" class="btn btn-primary">Sign in</button>
+          </form>
+          <a class="btn btn-primary" href="register.php" role="button">Register</a>
+        </div>
+
+
     </div>
   </nav>
   </div>
