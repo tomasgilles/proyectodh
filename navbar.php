4d353a301806865e4bb72af5af912f057ae90ed2
tomasgilles <tomas.gilles@gmail.com>
2019-09-12 00:26:38 -0300

    Micarrito

--- navbar.php
+++ navbar.php
@@ -45,14 +45,12 @@
         </ul>
         <form class="form-inline my-2 my-lg-0" id="formnavbar">
           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
         </form>
-
         <div class="dropdown">
           <button class="btn btn-primary dropdown-toggle" type="button" id="loginFromButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Login
           </button>
-          <form class="dropdown-menu dropdown-menu p-4" aria-labelledby="loginFromButton">
+          <form class="dropdown-menu dropdown-menu p-4" aria-labelledby="loginFromButton" id=logindropdown>
             <div class="form-group">
               <label for="exampleDropdownFormEmail2">Email address</label>
               <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
@@ -70,9 +68,8 @@
             <button type="submit" class="btn btn-primary">Sign in</button>
           </form>
           <a class="btn btn-primary" href="register.php" role="button">Register</a>
+          <a class="btn btn-primary" href="register.php" role="button"><i id= "micarrito" class="fas fa-shopping-cart"></i></i></a>
         </div>
-
-
     </div>
   </nav>
   </div>
