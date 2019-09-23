da79492607c1998cd695aac252da75088b78d11a
Marcos <marcos.letemendia@gmail.com>
2019-09-23 11:12:21 -0300

    que persistan los datos en registro con si hay errores (registro)

--- register.php
+++ register.php
@@ -1,11 +1,15 @@
 <?php
-$errores=[];
 include ("funciones.php");
 $errores=[];
 $nombreOk="";
+$apellidoOk="";
 $emailOk="";
 
 if ($_POST) {
+  $nombreOk=trim($_POST["nombre"]);
+  $apellidoOk=trim($_POST["apellido"]);
+  $emailOk=trim($_POST["email"]);
+
   $errores=validarRegistro($_POST);
 
   if (!$errores) {
@@ -44,15 +48,27 @@ if ($_POST) {
             <?php endif ?>
             <div class="form-group">
               <label for="formGroupExampleInput"></label>
-              <input type="text" name="nombre" class="form-control" id="formGroupExampleInput" placeholder="Nombre*" required>
+              <?php if(isset($errores["nombre"])): ?>
+                <input type="text" name="nombre" class="form-control" id="formGroupExampleInput" placeholder="Nombre*" value="" required>
+              <?php else: ?>
+                <input type="text" name="nombre" class="form-control" id="formGroupExampleInput" placeholder="Nombre*" value="<?= $nombreOk ?>" required>
+              <?php endif ?>
             </div>
             <div class="form-group">
               <label for="formGroupExampleInput2"></label>
-              <input type="text" name="apellido" class="form-control" id="formGroupExampleInput2" placeholder="Apellido*" required>
+              <?php if(isset($errores["apellido"])): ?>
+                <input type="text" name="apellido" class="form-control" id="formGroupExampleInput2" placeholder="Apellido*" value="" required>
+              <?php else : ?>
+                <input type="text" name="apellido" class="form-control" id="formGroupExampleInput2" placeholder="Apellido*" value="<?= $apellidoOk ?>" required>
+              <?php endif ?>
             </div>
             <div class="form-group">
               <label for="formGroupExampleInput2"></label>
-              <input type="email" name="email" class="form-control" id="formGroupExampleInput3" placeholder="Email*" required>
+              <?php if(isset($errores["email"])): ?>
+                <input type="email" name="email" class="form-control" id="formGroupExampleInput3" placeholder="Email*" value="" required>
+              <?php else: ?>
+                <input type="email" name="email" class="form-control" id="formGroupExampleInput3" placeholder="Email*" value="<?= $emailOk ?>" required>
+              <?php endif ?>
             </div>
             <div class="form-group">
               <label for="formGroupExampleInput2"></label>
