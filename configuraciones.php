741b2598b790d0b600b2f4a7a6ac522dd4d6b60e
Marcos <marcos.letemendia@gmail.com>
2019-11-05 12:12:39 -0300

    proyecto laravel

--- configuraciones.php
+++ configuraciones.php
@@ -1,5 +1,4 @@
 <meta charset="utf-8">
-<meta charset="utf-8">
 <link rel="stylesheet" href="css/estilosnav.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
